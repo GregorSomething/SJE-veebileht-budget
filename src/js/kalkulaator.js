ac04533c487c8b96224a8675888cfa4c0516ba20
Gregor Suurvarik <55884713+GregorSomething@users.noreply.github.com>
2022-11-17 10:57:35 +0200

    Lisasin automaatse sisendi kuulamise

--- src/js/kalkulaator.js
+++ src/js/kalkulaator.js
@@ -1,1 +1,25 @@
- 
+// Autor: Gregor, kogu fail, va millel on muu viitamine.
+
+window.onload = function () {
+    let data = {
+        "in": {}
+    }
+
+    // NB lisa kõik kuulajad, või tee automaatseks sisendi kuulamine
+    Array.from(document.getElementById("user-input").getElementsByTagName("input")).forEach(element => {
+        if (element.type == "number") {
+            element.addEventListener('change', (evt) => {
+                updateAndRender(parseFloat(evt.target.value), evt.target.id)});
+            data.in[element.id] = parseFloat(element.value)
+        }
+    });
+
+    const updateAndRender = (value, id) => {
+        data.in[id] = value
+        render()
+    }
+
+    const render = () => {
+        console.log(data)
+    }
+}  
